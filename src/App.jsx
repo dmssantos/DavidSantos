b69cc7139e062d15239dbf3f0c3ef8f05e8f5592
dmssantos <dms.santos_@hotmail.com>
2022-02-04 20:22:32 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,8 @@
-import styled from "styled-components";
 import Globalstyle from "./Global-Style";
 import Routing from "./routes/index";
 import Menu from "./components/Menu/index";
 import logo from "./utils/images/logo-menor.svg";
-
-const WrapperApp = styled.div`
-  width: 100%;
-  height: calc(100vh - 100px);
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-
-  div.wrapperlogo {
-    width: 100%;
-
-    img.logo-menor {
-      align-self: flex-start;
-    }
-  }
-
-  div.content {
-    width: 100%;
-    height: calc(100vh - 250px);
-    padding: 10px;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-`;
+import WrapperApp from "./style";
 
 function App() {
   return (
---
+++ src/style.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const WrapperApp = styled.div`
+  width: 100%;
+  height: calc(100vh - 100px);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  div.wrapperlogo {
+    width: 100%;
+
+    img.logo-menor {
+      align-self: flex-start;
+    }
+  }
+
+  div.content {
+    width: 100%;
+    height: calc(100vh - 250px);
+    padding: 10px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export default WrapperApp;
