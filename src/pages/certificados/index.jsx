f9caeb90e04af2ac12a3bc6a667c7727b15b7d15
dmssantos <dms.santos_@hotmail.com>
2022-02-19 10:28:15 -0300

    lazy

--- src/pages/certificados/index.jsx
+++ src/pages/certificados/index.jsx
@@ -4,28 +4,28 @@ import * as Cert from "../../utils/images/certifications/images";
 function Certificados() {
   return (
     <WrapperCertificados>
-      <img src={Cert.image1} alt="image1" />
-      <img src={Cert.image2} alt="image2" />
-      <img src={Cert.image3} alt="image3" />
-      <img src={Cert.image4} alt="image4" />
-      <img src={Cert.image5} alt="image5" />
-      <img src={Cert.image6} alt="image6" />
-      <img src={Cert.image7} alt="image7" />
-      <img src={Cert.image8} alt="image8" />
-      <img src={Cert.image9} alt="image9" />
-      <img src={Cert.image11} alt="image11" />
-      <img src={Cert.image12} alt="image12" />
-      <img src={Cert.image13} alt="image13" />
-      <img src={Cert.image14} alt="image14" />
-      <img src={Cert.image15} alt="image15" />
-      <img src={Cert.image16} alt="image16" />
-      <img src={Cert.image17} alt="image17" />
-      <img src={Cert.image18} alt="image18" />
-      <img src={Cert.image19} alt="image19" />
-      <img src={Cert.image21} alt="image21" />
-      <img src={Cert.image22} alt="image22" />
-      <img src={Cert.image23} alt="image23" />
-      <img src={Cert.image24} alt="image24" />
+      <img src={Cert.image1} alt="image1" loading="lazy" />
+      <img src={Cert.image2} alt="image2" loading="lazy" />
+      <img src={Cert.image3} alt="image3" loading="lazy" />
+      <img src={Cert.image4} alt="image4" loading="lazy" />
+      <img src={Cert.image5} alt="image5" loading="lazy" />
+      <img src={Cert.image6} alt="image6" loading="lazy" />
+      <img src={Cert.image7} alt="image7" loading="lazy" />
+      <img src={Cert.image8} alt="image8" loading="lazy" />
+      <img src={Cert.image9} alt="image9" loading="lazy" />
+      <img src={Cert.image11} alt="image11" loading="lazy" />
+      <img src={Cert.image12} alt="image12" loading="lazy" />
+      <img src={Cert.image13} alt="image13" loading="lazy" />
+      <img src={Cert.image14} alt="image14" loading="lazy" />
+      <img src={Cert.image15} alt="image15" loading="lazy" />
+      <img src={Cert.image16} alt="image16" loading="lazy" />
+      <img src={Cert.image17} alt="image17" loading="lazy" />
+      <img src={Cert.image18} alt="image18" loading="lazy" />
+      <img src={Cert.image19} alt="image19" loading="lazy" />
+      <img src={Cert.image21} alt="image21" loading="lazy" />
+      <img src={Cert.image22} alt="image22" loading="lazy" />
+      <img src={Cert.image23} alt="image23" loading="lazy" />
+      <img src={Cert.image24} alt="image24" loading="lazy" />
     </WrapperCertificados>
   );
 }
